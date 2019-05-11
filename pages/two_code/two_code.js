6b57e9bf82e875cd0018b9f68358204abe12612e
zhou136 <2980765157@qq.com>
2019-05-11 09:54:10 +0800

    CheHuiwanji
    修改了团长，以及二维码推广页面

--- pages/two_code/two_code.js
+++ pages/two_code/two_code.js
@@ -1,3 +1,23 @@
+import {Api} from '../../utils/api.js';
+var api = new Api();
+const app = getApp();
+import {Token} from '../../utils/token.js';
+const token = new Token();
+
 Page({
-	
+  data: {
+    
+  },
+  onLoad: function () {
+   
+    
+  },
+	intoPathRedirect(e){
+	  const self = this;
+	  api.pathTo(api.getDataSet(e,'path'),'redi');
+	},
+	intoPath(e){
+	  const self = this;
+	  api.pathTo(api.getDataSet(e,'path'),'nav');
+	}
 })
